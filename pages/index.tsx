8c01dd43d4f6b8211e8e4fc475aa74cd3661399b
ikeda1729 <d.enkikairo@gmail.com>
2022-10-06 23:21:48 +0900

    adds user tweets page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,9 @@
 import type { NextPage } from "next"
-import Head from "next/head"
 import LoginPage from "./login"
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>NextGoApp</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       {/* <LoginPage/> */}
     </div>
   )
